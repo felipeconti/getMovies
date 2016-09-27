2cd03fe725bccd1101cf623e089215248d0e17c0
Felipe Bonvicini Conti <felipe.conti@SPOD3403.local>
2016-09-27 19:52:42 -0300

    Initial webapp

---
+++ app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name getMoviesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the getMoviesApp
+ */
+angular.module('getMoviesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
