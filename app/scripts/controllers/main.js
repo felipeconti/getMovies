46f1c20be576fd2030f827424a3e6e1771c910cc
Felipe Conti <felipeconti18@gmail.com>
2016-10-05 22:37:31 -0300

    Just simple quotation marks

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,18 +22,18 @@ app.controller('MainCtrl', function ($scope, $http, ngProgressFactory) {
   $scope.uploadFile = function(files) {
     var fd = new FormData();
     //Take the first selected file
-    fd.append("files", files[0]);
+    fd.append('files', files[0]);
 
     // var fd = new FormData()
     // for (var i in $scope.files) {
     //     fd.append("uploadedFile", $scope.files[i])
     // }
     var xhr = new XMLHttpRequest()
-    xhr.upload.addEventListener("progress", uploadProgress, false)
-    xhr.addEventListener("load", uploadComplete, false)
-    xhr.addEventListener("error", uploadFailed, false)
-    xhr.addEventListener("abort", uploadCanceled, false)
-    xhr.open("POST", "http://localhost:8080/fileupload")
+    xhr.upload.addEventListener('progress', uploadProgress, false)
+    xhr.addEventListener('load', uploadComplete, false)
+    xhr.addEventListener('error', uploadFailed, false)
+    xhr.addEventListener('abort', uploadCanceled, false)
+    xhr.open('POST', 'http://localhost:8080/fileupload')
     xhr.send(fd)
   };
 
@@ -49,10 +49,10 @@ app.controller('MainCtrl', function ($scope, $http, ngProgressFactory) {
     console.log(evt.target.responseText)
   }
   function uploadFailed(evt) {
-    console.log("There was an error attempting to upload the file.")
+    console.log('There was an error attempting to upload the file.')
   }
   function uploadCanceled(evt) {
     $scope.contained_progressbar.reset();
-    console.log("The upload has been canceled by the user or the browser dropped the connection.")
+    console.log('The upload has been canceled by the user or the browser dropped the connection.')
   }
 });
